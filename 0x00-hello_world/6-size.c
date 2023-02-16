9d8f5147675d04bcaaff3ab0fcf007c842258a4c
Chibike98 <chibuikemegbo@gmail.com>
2023-02-16 11:47:29 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various types on the computer compiled on
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float h;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a);
