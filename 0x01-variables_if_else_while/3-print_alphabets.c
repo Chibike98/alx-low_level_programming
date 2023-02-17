f6b49c9e9918bcd6ba257b5cab1483a88bb8c439
Chibike98 <chibuikemegbo@gmail.com>
2023-02-17 15:12:16 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+/**
+ * main - Entry point
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar(c);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
