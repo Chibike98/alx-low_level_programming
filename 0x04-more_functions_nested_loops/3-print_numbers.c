886b8e3bf6f539e0a440827bd966b0571c837957
Chibike98 <chibuikemegbo@gmail.com>
2023-02-23 15:59:06 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
 /**
  * print_numbers - print 0-9
  * Return: void
  */
- void print_numbers(void)
+
+void print_numbers(void)
 {
 	char c;
 
@@ -13,4 +13,5 @@
 		_putchar(c);
 	}
 	_putchar('\n');
+	return (0);
 }
