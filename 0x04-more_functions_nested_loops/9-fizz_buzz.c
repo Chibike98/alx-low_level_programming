22e6fc29b3e3fe28626faa3b2b273d9dd6bc00fa
Chibike98 <chibuikemegbo@gmail.com>
2023-02-24 02:32:04 -0800

    Fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,23 +4,24 @@
  * main - Entry point
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz");
 		}
 		else
 		{
