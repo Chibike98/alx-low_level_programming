a5735a39990fd2743e380b4e02de13af48f0a659
Chibike98 <chibuikemegbo@gmail.com>
2023-02-23 16:40:00 -0800

    numers git commit -m numers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,19 +1,24 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * main - check the code
+ * print_number - Prints an integer
+ * @n: integer to print
  * Return: 0
  */
-int main(void)
+
+void print_number(int n)
 {
-	print_number(98);
-	_putchar('\n');
-	print_number(402);
-        _putchar('\n');
-	print_number(1024);
-        _putchar('\n');
-	print_number(0);
-        _putchar('\n');
-	print_number(98);
-        _putchar('\n');
-	return (0);
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		k = n;
+		_putchar('_');
+	}
+	k /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
 }
