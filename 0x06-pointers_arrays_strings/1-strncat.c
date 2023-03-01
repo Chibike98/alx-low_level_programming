4917a9bb1e12e16416a82f649e9c06c04cd32adf
Chibike98 <chibuikemegbo@gmail.com>
2023-03-01 13:32:37 -0800

    stringcat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncat - joins two strings using an inputted number of bytes from src
+ * @dest: string to be appended on
+ * @src: string to be appended to dest
+ * @n: number of btyes
+ * Return: pointer to resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index++] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
