23c8e22e50631fb6ab1bc495882aad71c7ed0a42
Chibike98 <chibuikemegbo@gmail.com>
2023-03-02 11:28:57 -0800

    Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * rot13 - encodes a string using rot13
- * @str: string to be encoded
+ * @s: string to be encoded
  * Return: point to encoded string
  */
 char *rot13(char *s)
