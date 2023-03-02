d4bd4946198599b44a84ae72be47e4083bc7b90b
Chibike98 <chibuikemegbo@gmail.com>
2023-03-02 11:27:38 -0800

    Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,35 +4,24 @@
  * @str: string to be encoded
  * Return: point to encoded string
  */
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int index1 = 0, index2;
-	char alphabet[52] = {
-		'A', 'B', 'C', 'D', 'E', 'F',
-		'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-		'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-		'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
-		'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-		'u', 'v', 'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B',
-		'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-		'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't',
-		'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c',
-		'd', 'e', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] =
+		"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (str[index1])
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (index2 = 0; index2 < 52; index2++)
+		for (j = 0; j < 52; j++)
 		{
-			if (str[index1] == alphabet[index2])
+			if (s[i] == data1[j])
 			{
-				str[index1] = rot13key[index2];
+				s[i] = datarot[j];
 				break;
 			}
 		}
-		index1++;
 	}
-	return (str);
+	return (s);
 }
