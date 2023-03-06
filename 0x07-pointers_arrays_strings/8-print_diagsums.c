86714b38bcd748acc0c39d5b71d6b0e98413fb61
Chibike98 <chibuikemegbo@gmail.com>
2023-03-06 14:55:44 -0800

    diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,7 +9,7 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int sum1, um2, y;
+	int sum1, sum2, y;
 
 	sum1 = 0;
 	sum2 = 0;
@@ -18,7 +18,7 @@ void print_diagsums(int *a, int size)
 	{
 		sum1 = sum1 + a[y * size + y];
 	}
-	for (y = sie - 1; y >= 0; y--)
+	for (y = size - 1; y >= 0; y--)
 	{
 		sum2 += a[y * size + (size - y - 1)];
 	}
