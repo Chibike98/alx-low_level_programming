18b0088e49ae18d1e5ae411668757d2d01554a3a
Chibike98 <chibuikemegbo@gmail.com>
2023-03-08 09:14:20 -0800

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,10 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*s)
+	if (*s == '\0')
+		return (0);
+	else
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+	       return (1 + _strlen_recursion(s + 1));
 	}
-	return (*s);
 }
