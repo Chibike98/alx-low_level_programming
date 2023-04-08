378e96382cd50646999d3a4162f1e27740ee7915
Chibike98 <chibuikemegbo@gmail.com>
2023-04-08 14:12:30 -0700

    bitwise

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: number to search
+ * @index: index of the bit
+ * Return:  the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1;
+	mask <<= index;
+
+	if (index > 63)
+		return (-1);
+	if (n & mask)
+		return (1);
+	else
+		return (0);
+}
