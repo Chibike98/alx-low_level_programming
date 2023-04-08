eff0f150dc3802bc049dd4bb28a7d83dc81b731a
Chibike98 <chibuikemegbo@gmail.com>
2023-04-08 13:53:48 -0700

    bitwise

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,24 +1,35 @@
 #include "main.h"
 /**
  * print_binary - print binary rep of a number
+ * @n: no to print in binary
  * Return: 0
  */
 void print_binary(unsigned long int n)
 {
-	int bit_count = sizeof(unsigned long int) * 8;
-	unsigned long int a = (unsigned long int)1 << (bit_count - 1);
+	int a = 0, count = 0;
+	int binary[64];
 
-	while (a > 0)
+	if (n == 0)
 	{
-		if (n & a)
-		{
-			printf("1");
-		}
-		else
-		{
-			printf("0");
-		}
-		a >>= 1;
+		_putchar('0');
+		return;
 	}
-	printf("\n");
+	while (n > 0)
+	{
+		binary[a] = n % 2;
+		n /= 2;
+		a++;
+	}
+	for (int j = a - 1; j >= 0; j--)
+	{
+		if (binary[j] == 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count > 0)
+			_putchar('0');
+	}
+	if (count == 0)
+		_putchar('0');
 }
